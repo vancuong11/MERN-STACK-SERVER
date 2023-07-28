54468b6b813150a3105acbc5d890e5e672a449b0
vancuong11 <cuongg121101@gmail.com>
2023-07-28 12:25:35 +0700

    #16 Thiết kế DB và connect với MongooseDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// connect mongoose DB
+mongoose
+    .connect('mongodb://localhost:27017')
+    .then(() => {
+        console.log('connection successfully!');
+    })
+    .catch((e) => {
+        console.log('connection error', e);
+    });
 
 app.get('/', (req, res) => {
     res.send('Hello World');
---
+++ src/models/OrderProduct.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const oderSchema = new mongoose.Schema(
+    {
+        orderItems: [
+            {
+                name: { type: String, required: true },
+                amount: { type: Number, required: true },
+                image: { type: String, required: true },
+                price: { type: Number, required: true },
+                // join table product into orderProduct
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'Product',
+                    required: true,
+                },
+            },
+        ],
+        shippingAddress: {
+            fullname: { type: String, required: true },
+            address: { type: String, required: true },
+            city: { type: String, required: true },
+            country: { type: String, required: true },
+            phone: { type: String, required: true },
+        },
+        paymentMethods: { type: String, required: true },
+        itemsPrice: { type: Number, required: true },
+        shippingPrice: { type: Number, require: true },
+        taxPrice: { type: Number, require: true },
+        totalPrice: { type: Number, require: true },
+        // join table user into orderProduct
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            require: true,
+        },
+        isPaid: { type: Boolean, default: false },
+        paidAt: { type: Date },
+        isDelivered: { type: Boolean, default: false },
+        deliveredAt: { type: Date },
+    },
+    {
+        timestamps: true,
+    },
+);
+const OrderProduct = mongoose.model('OrderProduct', oderSchema);
+module.exports = OrderProduct;
